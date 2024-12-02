cf58971926aaa5fc5bcdf24cd85a09690e0d157d
llEraserheadll <abinash1.sahu37@gmail.com>
2024-12-02 06:23:41 +0000

    Code challenge submission for question: 121 Best Time to Buy and Sell Stock

---
+++ 121 Best Time to Buy and Sell Stock/121best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy_price = prices[0]
+        profit = 0
+
+        for p in prices[1:]:
+            if p < buy_price:
+                buy_price = p
+            
+            profit = max(profit,p-buy_price)
+        
+        return profit
+
+        
