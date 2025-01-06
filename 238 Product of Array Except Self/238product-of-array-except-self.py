d980b67ca9ce887a5874fba86a187021f9d740ac
llEraserheadll <abinash1.sahu37@gmail.com>
2025-01-06 11:14:18 +0000

    Code challenge submission for question: 238 Product of Array Except Self

--- 238 Product of Array Except Self/238product-of-array-except-self.py
+++ 238 Product of Array Except Self/238product-of-array-except-self.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        l_mult = 1
+        r_mult = 1
         n = len(nums)
-        answer = [1] * n
+        l_arr = [0]*n
+        r_arr = [0]*n
 
-        left = 1
         for i in range(n):
-            answer[i] = left
-            left *= nums[i]
+            j = -i -1
+            l_arr[i] = l_mult
+            r_arr[j] = r_mult
+            l_mult *= nums[i]
+            r_mult *= nums[j]
 
-        right = 1
-        for i in range(n-1,-1,-1):
-            answer[i] *= right
-            right *= nums[i]
-        return answer 
+        return [l*r for l,r in zip(l_arr,r_arr)]    
