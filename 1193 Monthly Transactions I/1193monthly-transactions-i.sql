fba2c4d50a7ed20c03847dcb480a725230942b29
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-04 14:13:28 +0000

    Code challenge submission for question: 1193 Monthly Transactions I

--- 1193 Monthly Transactions I/1193monthly-transactions-i.sql
+++ 1193 Monthly Transactions I/1193monthly-transactions-i.sql
@@ -1,9 +1,32 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- select left(trans_date,7) as month,
+-- country,
+-- count(state) as trans_count,
+-- sum(if(state='approved',1,0)) as approved_count,
+-- sum(amount) as trans_total_amount,
+-- sum(if(state='approved',amount,0))as approved_total_amount
+-- from Transactions
+-- group by month,country
+
+
+
+
+
+
+
+
+
+
+
 select left(trans_date,7) as month,
 country,
-count(id) as trans_count,
+count(amount) as trans_count,
 sum(if(state = 'approved',1,0)) as approved_count,
 sum(amount) as trans_total_amount,
-sum((state='approved') * amount) as approved_total_amount
+sum(if(state = 'approved',amount,0)) as approved_total_amount
 from Transactions
 group by month,country
+
+
+
+
