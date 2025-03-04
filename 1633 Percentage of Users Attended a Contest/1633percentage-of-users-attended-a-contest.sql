e28df1b418c4b8e20618f150599f7e48486b08e3
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-04 13:44:23 +0000

    Code challenge submission for question: 1633 Percentage of Users Attended a Contest

--- 1633 Percentage of Users Attended a Contest/1633percentage-of-users-attended-a-contest.sql
+++ 1633 Percentage of Users Attended a Contest/1633percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,21 @@
-# Write your MySQL query statement belos
-select contest_id, round(count(user_id)*100/(select count(user_id) from Users),2) as percentage
-from Register
-group by contest_id
-order by percentage desc,contest_id
+-- # Write your MySQL query statement below
+-- select contest_id,round(count(user_id)*100/(select count(user_id) from Users),2) as percentage from Register r 
+-- group by contest_id order by percentage desc,contest_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select contest_id , round(count(user_id) * 100 / (select count(user_id) from Users),2) as percentage from Register
+group by contest_id order by percentage desc,contest_id
