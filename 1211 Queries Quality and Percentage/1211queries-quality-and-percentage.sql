cad89b38a401fd420217c7dae92b7a76828e659c
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-04 13:58:13 +0000

    Code challenge submission for question: 1211 Queries Quality and Percentage

--- 1211 Queries Quality and Percentage/1211queries-quality-and-percentage.sql
+++ 1211 Queries Quality and Percentage/1211queries-quality-and-percentage.sql
@@ -1,3 +1,28 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- select query_name,
+-- round(sum(rating/position)/count(query_name),2) as quality, 
+-- round(avg(if(rating<3,1,0))*100,2) as poor_query_percentage 
+-- from Queries 
+-- where query_name is not null
+-- group by query_name
 
-select query_name, round(avg(rating/position),2) as quality, round(sum(if(rating<3,1,0))*100/ count(rating),2) as poor_query_percentage from queries where query_name is not null group by query_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select query_name ,
+round(sum(rating/position)/count(query_name),2) as quality,
+round(avg(if(rating<3,1,0))*100,2) as poor_query_percentage
+from Queries 
+group by query_name
