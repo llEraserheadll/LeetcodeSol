a58280d307a5798b1d8dce4ac2e9506f2058eea6
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-05 03:53:04 +0000

    Code challenge submission for question: 596 Classes More Than 5 Students

---
+++ 596 Classes More Than 5 Students/596classes-more-than-5-students.sql
@@ -0,0 +1,18 @@
+-- # Write your MySQL query statement below
+-- select class from Courses group by class having count(student)>=5 
+
+
+
+
+
+
+
+
+
+
+
+select class from Courses group by class having count(student) >= 5
+
+
+
+
