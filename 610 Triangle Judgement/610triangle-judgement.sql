8148f132068f5e47fcf4a796f7fe1fab60708310
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-05 05:24:15 +0000

    Code challenge submission for question: 610 Triangle Judgement

---
+++ 610 Triangle Judgement/610triangle-judgement.sql
@@ -0,0 +1,17 @@
+-- # Write your MySQL query statement below
+-- select *, if(x+y>z and x+z>y and y+z>x,'Yes','No') as triangle from Triangle
+
+
+
+
+
+
+
+
+
+
+select * , if(x+y>z and y+z>x and x+z>y,'Yes','No') as triangle from Triangle
+
+
+
+
