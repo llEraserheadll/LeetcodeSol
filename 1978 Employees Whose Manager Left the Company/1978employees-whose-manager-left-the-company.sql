b977a372f87b3d9c64dfb1459250695d8e0fde2c
llEraserheadll <abinash1.sahu37@gmail.com>
2025-03-06 00:52:34 +0000

    Code challenge submission for question: 1978 Employees Whose Manager Left the Company

---
+++ 1978 Employees Whose Manager Left the Company/1978employees-whose-manager-left-the-company.sql
@@ -0,0 +1,18 @@
+-- # Write your MySQL query statement below
+-- select employee_id from Employees where salary <30000 and manager_id is not null and manager_id not in(select employee_id from employees) order by employee_id
+
+
+
+
+
+select employee_id from Employees where salary < 30000 and manager_id not in (select employee_id from Employees)
+order by employee_id
+
+
+
+
+
+
+
+
+
